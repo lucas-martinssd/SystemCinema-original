08804c56d24aa04cb6b136e499222bc75c83f750
Lucas Martins <lucassantosm711@gmail.com>
2024-05-08 20:56:22 -0300

    Atualização

--- src/main/java/com/mycompany/systemcinema/Sistema.java
+++ src/main/java/com/mycompany/systemcinema/Sistema.java
@@ -4,27 +4,37 @@
  */
 package com.mycompany.systemcinema;
 
-import java.time.Duration;
 import java.util.Scanner;
 
 /**
+ * Classe para gerenciar o sistema do cinema.
+ * <p>
+ * Esta classe contém métodos para interação com diferentes tipos de usuários, como clientes, funcionários e proprietários,
+ * fornecendo opções específicas de acordo com o tipo de usuário.
+ * </p>
  *
  * @author Lucas
+ * @version 1.0
  */
 public class Sistema {
-    
-    //private Cliente cliente;
-    //private Sessao sessao;
-    //private GestaoDeFilmes gestaoDeFilmes;
-    //private Carrinho carrinho;
-    //private Estoque estoque;
-    //private Funcionario funcionario;
-    //private GestaoDeClientes gestaoDeClientes;
-    
+
     Scanner sc = new Scanner(System.in);
-    
-    public void informarClienteOuFuncionarioOuProprietario(Cliente cliente, Sessao sessao, GestaoDeFilmes gestaoDeFilmes, 
-            Carrinho carrinho, Estoque estoque, Funcionario funcionario, GestaoDeClientes gestaoDeClientes, Proprietario proprietario) {
+
+    /**
+     * Método para informar ao sistema se o usuário é cliente, funcionário ou proprietário, e fornecer opções de ação de acordo com o tipo de usuário.
+     *
+     * @param cliente          O cliente que está acessando o sistema.
+     * @param sessao           A sessão atual do cinema.
+     * @param gestaoDeFilmes   O gerenciador de filmes do cinema.
+     * @param carrinho         O carrinho de compras do cliente.
+     * @param estoque          O estoque de produtos do cinema.
+     * @param funcionario      O funcionário que está acessando o sistema.
+     * @param gestaoDeClientes O gerenciador de clientes do cinema.
+     * @param proprietario     O proprietário do cinema.
+     */
+    public void informarClienteOuFuncionarioOuProprietario(Cliente cliente, Sessao sessao, GestaoDeFilmes gestaoDeFilmes,
+                                                           Carrinho carrinho, Estoque estoque, Funcionario funcionario,
+                                                           GestaoDeClientes gestaoDeClientes, Proprietario proprietario) {
         System.out.println("Você é Cliente, Funcionario ou Proprietario? ");
         String funcao = sc.nextLine();
         String loginClienteOriginal = cliente.getLoginClienteOriginal();
@@ -33,134 +43,107 @@ public class Sistema {
         String senhaFuncionarioOriginal = funcionario.getSenhaFuncionarioOriginal();
         String loginProprietarioOriginal = proprietario.getLoginProprietarioOriginal();
         String senhaProprietarioOriginal = proprietario.getSenhaProprietarioOriginal();
-        
+
         if (funcao.equals("Cliente")) {
             System.out.println("Informe o seu login: ");
             String loginCliente = sc.nextLine();
             System.out.println("Informe a sua senha: ");
             String senhaCliente = sc.nextLine();
-            
-            if (loginCliente.equals(loginClienteOriginal) && senhaCliente.equals(senhaClienteOriginal)){
+
+            if (loginCliente.equals(loginClienteOriginal) && senhaCliente.equals(senhaClienteOriginal)) {
                 System.out.println("Bem vindo ao CineDjamas!\n Escolha a sua ação: Selecionar Filme, Selecionar Produtos");
                 String acaoCliente = sc.nextLine();
-                
-                if (acaoCliente.equals("Selecionar Filme")){
+
+                if (acaoCliente.equals("Selecionar Filme")) {
                     carrinho.selecionarFilme(gestaoDeFilmes);
-                }
-                else if (acaoCliente.equals("Selecionar Produtos")) {
+                } else if (acaoCliente.equals("Selecionar Produtos")) {
                     carrinho.selecionarProduto(estoque);
-                }
-                else {
+                } else {
                     System.out.println("Função não existente");
                 }
-            }
-            else {
+            } else {
                 System.out.println("Login ou senha incorretos.");
-                informarClienteOuFuncionarioOuProprietario(cliente, sessao, gestaoDeFilmes, carrinho, estoque, 
+                informarClienteOuFuncionarioOuProprietario(cliente, sessao, gestaoDeFilmes, carrinho, estoque,
                         funcionario, gestaoDeClientes, proprietario);
             }
-        }
-        else if (funcao.equals("Funcionario")) {
+        } else if (funcao.equals("Funcionario")) {
             System.out.println("Informe o seu login: ");
             String loginFuncionario = sc.nextLine();
             System.out.println("Informe a sua senha: ");
             String senhaFuncionario = sc.nextLine();
-            
-            if (loginFuncionario.equals(loginFuncionarioOriginal) && senhaFuncionario.equals(senhaFuncionarioOriginal)){
+
+            if (loginFuncionario.equals(loginFuncionarioOriginal) && senhaFuncionario.equals(senhaFuncionarioOriginal)) {
                 System.out.println("Bem vindo ao CineDjamas!\n Escolha a sua ação: Cadastrar Cliente, "
                         + "Editar Cadastro Cliente, Cadastrar Filmes, Remover Filmes, "
                         + "Remover Produto, Listar Produtos, Selecionar Filme, Selecionar Produto, Cadastrar Funcionario");
                 String acaoFuncionario = sc.nextLine();
-                
+
                 if (acaoFuncionario.equals("Cadastrar Cliente")) {
                     gestaoDeClientes.cadastroDeCliente(cliente);
-                }
-                else if (acaoFuncionario.equals("Editar Cadastro Cliente")){
+                } else if (acaoFuncionario.equals("Editar Cadastro Cliente")) {
                     gestaoDeClientes.editarCadastroCliente(cliente);
-                }
-                else if (acaoFuncionario.equals("Cadastrar Filme")){
+                } else if (acaoFuncionario.equals("Cadastrar Filme")) {
                     gestaoDeFilmes.cadastraFilme();
-                }
-                else if (acaoFuncionario.equals("Remover Filmes")){
+                } else if (acaoFuncionario.equals("Remover Filmes")) {
                     gestaoDeFilmes.removeFilme();
-                }
-                else if (acaoFuncionario.equals("Remover Produto")){
+                } else if (acaoFuncionario.equals("Remover Produto")) {
                     estoque.removerProduto();
-                }
-                else if (acaoFuncionario.equals("Listar Produtos")){
+                } else if (acaoFuncionario.equals("Listar Produtos")) {
                     estoque.listarProdutos();
-                }
-                else if (acaoFuncionario.equals("Selecionar Filme")){
+                } else if (acaoFuncionario.equals("Selecionar Filme")) {
                     carrinho.selecionarFilme(gestaoDeFilmes);
-                }
-                else if (acaoFuncionario.equals("Selecionar Produto")){
+                } else if (acaoFuncionario.equals("Selecionar Produto")) {
                     carrinho.selecionarProduto(estoque);
-                }
-                else if (acaoFuncionario.equals("Cadastrar Funcionario")){
+                } else if (acaoFuncionario.equals("Cadastrar Funcionario")) {
                     funcionario.funcionarioRealizarCadastro();
-                }
-                else {
+                } else {
                     System.out.println("Função não existente");
                 }
-            }
-            else {
+            } else {
                 System.out.println("Login ou senha incorretos.");
-                informarClienteOuFuncionarioOuProprietario(cliente, sessao, gestaoDeFilmes, carrinho, estoque, 
+                informarClienteOuFuncionarioOuProprietario(cliente, sessao, gestaoDeFilmes, carrinho, estoque,
                         funcionario, gestaoDeClientes, proprietario);
             }
-        }
-        else if (funcao.equals("Proprietario")) {
+        } else if (funcao.equals("Proprietario")) {
             System.out.println("Informe o seu login: ");
             String loginProprietario = sc.nextLine();
             System.out.println("Informe a sua senha: ");
             String senhaProprietario = sc.nextLine();
-            
-            if (loginProprietario.equals(loginProprietarioOriginal) && senhaProprietario.equals(senhaProprietarioOriginal)){
+
+            if (loginProprietario.equals(loginProprietarioOriginal) && senhaProprietario.equals(senhaProprietarioOriginal)) {
                 System.out.println("Bem vindo ao CineDjamas!\n Escolha a sua ação: Registrar Proprietario, Comprar Estoque, Cadastrar Cliente, "
                         + "Editar Cadastro Cliente, Cadastrar Filmes, Remover Filmes, Remover Produto, Listar Produtos, Selecionar Filme, "
                         + "Selecionar Produto, Cadastrar Funcionario");
                 String acaoFuncionario = sc.nextLine();
-                
-                if (acaoFuncionario.equals("Registrar Proprietario")){
+
+                if (acaoFuncionario.equals("Registrar Proprietario")) {
                     proprietario.registroProprietario();
-                }
-                else if (acaoFuncionario.equals("Comprar Estoque")) {
+                } else if (acaoFuncionario.equals("Comprar Estoque")) {
                     proprietario.compraDeEstoque(estoque);
-                }
-                else if (acaoFuncionario.equals("Cadastrar Cliente")) {
+                } else if (acaoFuncionario.equals("Cadastrar Cliente")) {
                     gestaoDeClientes.cadastroDeCliente(cliente);
-                }
-                else if (acaoFuncionario.equals("Editar Cadastro Cliente")){
+                } else if (acaoFuncionario.equals("Editar Cadastro Cliente")) {
                     gestaoDeClientes.editarCadastroCliente(cliente);
-                }
-                else if (acaoFuncionario.equals("Cadastrar Filme")){
+                } else if (acaoFuncionario.equals("Cadastrar Filme")) {
                     gestaoDeFilmes.cadastraFilme();
-                }
-                else if (acaoFuncionario.equals("Remover Filmes")){
+                } else if (acaoFuncionario.equals("Remover Filmes")) {
                     gestaoDeFilmes.removeFilme();
-                }
-                else if (acaoFuncionario.equals("Remover Produto")){
+                } else if (acaoFuncionario.equals("Remover Produto")) {
                     estoque.removerProduto();
-                }
-                else if (acaoFuncionario.equals("Listar Produtos")){
+                } else if (acaoFuncionario.equals("Listar Produtos")) {
                     estoque.listarProdutos();
-                }
-                else if (acaoFuncionario.equals("Selecionar Filme")){
+                } else if (acaoFuncionario.equals("Selecionar Filme")) {
                     carrinho.selecionarFilme(gestaoDeFilmes);
-                }
-                else if (acaoFuncionario.equals("Selecionar Produto")){
+                } else if (acaoFuncionario.equals("Selecionar Produto")) {
                     carrinho.selecionarProduto(estoque);
-                }
-                else if (acaoFuncionario.equals("Cadastrar Funcionario")){
+                } else if (acaoFuncionario.equals("Cadastrar Funcionario")) {
                     funcionario.funcionarioRealizarCadastro();
-                }
-                else {
+                } else {
                     System.out.println("Função não existente");
                 }
-            }
-            else {
+            } else {
                 System.out.println("Login ou senha incorretos.");
-                informarClienteOuFuncionarioOuProprietario(cliente, sessao, gestaoDeFilmes, carrinho, estoque, 
+                informarClienteOuFuncionarioOuProprietario(cliente, sessao, gestaoDeFilmes, carrinho, estoque,
                         funcionario, gestaoDeClientes, proprietario);
             }
         }
